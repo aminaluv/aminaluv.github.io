7fc97af00ea45256048303176c79ae71a5ecf430
Amina <aminaluv@mit.edu>
2026-01-18 00:19:26 -0500

    Add more information to bullets

--- src/components/Bullet.tsx
+++ src/components/Bullet.tsx
@@ -1,10 +1,13 @@
 import "./Bullet.css";
-import blueberry1 from "../images/blueberry1.png";
 
-const Bullet = () => {
+interface BulletProps {
+  image: string;
+}
+
+const Bullet = ({image}: BulletProps) => {
   return (
     <div>
-      <img src={blueberry1} alt="blueberry1" className="blueberry" />
+      <img src={image} alt="blueberry1" className="blueberry" />
       {/* <div className="overlay-text">
         <span className="hello-text">hello! </span>
         <span className="name-text">i'm amina</span>
--- src/components/StickyNote.tsx
+++ src/components/StickyNote.tsx
@@ -1,6 +1,10 @@
 import "./StickyNote.css";
 import note from "../images/noteyellow.webp";
-import blueberry from "../images/blueberry1.png";
+import blueberry1 from "../images/blueberry1.png";
+import blueberry2 from "../images/blueberry2.png";
+import blueberry3 from "../images/blueberry3.png";
+import blueberry4 from "../images/blueberry4.png";
+
 import Bullet from "./Bullet";
 
 const AboutText = () => {
@@ -26,19 +30,28 @@ const AboutText = () => {
       </div>
       <div className="bullet-points-container">
         <div className="bullet-point">
-          <Bullet />
+          <Bullet image={blueberry1} />
           <span className="bullet-text">
-            Senior at MIT studying for a Bachelor of Science in Artificial
+            Senior at MIT pursuing a Bachelor of Science in Artificial
             Intelligence and Decision Making (6-4)
           </span>
         </div>
         <div className="bullet-point">
-          <Bullet />
+          <Bullet image={blueberry2} />
           <span className="bullet-text">Minoring in Management (15-1)</span>
         </div>
         <div className="bullet-point">
-          <Bullet />
-          <span className="bullet-text">Pursuing the New Engineering Education Transformation (NEET) Autonomous Machines certificate</span>
+          <Bullet image={blueberry3} />
+          <span className="bullet-text">
+            New Engineering Education Transformation (NEET) Autonomous Machines
+            certificate
+          </span>
+        </div>
+        <div className="bullet-point">
+          <Bullet image={blueberry4} />
+          <span className="bullet-text">
+            3x Amazon Software Development Engineering Intern
+          </span>
         </div>
       </div>
     </div>
