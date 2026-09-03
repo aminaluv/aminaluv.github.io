bed429388b2fb97e6fae028b94e35c34d4d9e71c
Amina <aminaluv@mit.edu>
2026-09-03 10:17:49 -0400

    Add deployment logic

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   async headers() {
     return [
